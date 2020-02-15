44d1b43b585b1c9a4aea4e06590fbe2e1c353932
erilu <35707834+erilu@users.noreply.github.com>
2020-02-14 22:28:59 -0800

    introduce data and download fastq files

---
+++ fastq_download.py
@@ -0,0 +1,23 @@
+import subprocess
+
+sra_numbers = [
+    "SRR7179504", "SRR7179505", "SRR7179506", "SRR7179507",
+    "SRR7179508", "SRR7179509", "SRR7179510", "SRR7179511",
+    "SRR7179520", "SRR7179521", "SRR7179522", "SRR7179523",
+    "SRR7179524", "SRR7179525", "SRR7179526", "SRR7179527",
+    "SRR7179536", "SRR7179537", "SRR7179540","SRR7179541"
+    ]
+
+# this will download the .sra files to ~/ncbi/public/sra/ (will create directory if not present)
+for sra_id in sra_numbers:
+    print ("Currently downloading: " + sra_id)
+    prefetch = "prefetch " + sra_id
+    print ("The command used was: " + prefetch)
+    subprocess.call(prefetch, shell=True)
+
+# this will extract the .sra files from above into a folder named 'fastq'
+for sra_id in sra_numbers:
+    print ("Generating fastq for: " + sra_id)
+    fastq_dump = "fastq-dump --outdir fastq --gzip --skip-technical  --readids --read-filter pass --dumpbase --split-3 --clip ~/ncbi/public/sra/" + sra_id + ".sra"
+    print ("The command used was: " + fastq_dump)
+    subprocess.call(fastq_dump, shell=True)
